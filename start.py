16b6e6a0544dc16f68609a228df3981516841b5e
Viacheslav Kirichenko <botinkakvd@gmail.com>
2019-03-24 19:56:22 +0300

    11

--- start.py
+++ start.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello from Docker container!"
+    return "I was built in jenkins and run in docker. "
 if __name__ == "__main__":
         app.run(threaded=True,debug=True,host='0.0.0.0')
